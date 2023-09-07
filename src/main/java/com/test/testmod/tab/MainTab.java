b19e9beae1de9c69454d3b178d976ef913d94764
Kurumimnm <2220397.ecc@gmail.com>
2023-09-07 14:40:07 +0900

    クリエイティブタブの追加

---
+++ src/main/java/com/test/testmod/tab/MainTab.java
@@ -0,0 +1,11 @@
+package com.test.testmod.tab;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+
+public class MainTab {
+    public static final Item[] items = {
+            Items.COMMAND_BLOCK,
+            Items.BARRIER
+    };
+}
---
+++ src/main/java/com/test/testmod/tab/Tabs.java
@@ -0,0 +1,29 @@
+package com.test.testmod.tab;
+
+import com.test.testmod.TestMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class Tabs {
+
+    public static final DeferredRegister<CreativeModeTab> MOD_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TestMod.MODID);
+
+    public static final RegistryObject<CreativeModeTab> TEST_MAIN = MOD_TABS.register("test_main",
+            () ->{
+                return CreativeModeTab.builder()
+                .icon(()->new ItemStack(Blocks.COMMAND_BLOCK))
+                .title(Component.translatable("itemGroup.test_main"))
+                .displayItems((param,output) -> {
+                    for(Item item:MainTab.items) {
+                        output.accept(item);
+                    }
+                })
+                .build();
+    });
+}
